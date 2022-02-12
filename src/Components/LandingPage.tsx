9fc2c542b9468da4969e691b27bfa46853a75465
KasperBaun <kasperbaun@hotmail.com>
2022-02-12 11:51:24 +0100

    docker

--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -4,7 +4,7 @@ const LandingPage = () => {
     return (
         <div>
             <h1>Landing Page</h1>
-            <p>Just testing stuff</p>
+            <p>Testing stuff</p>
         </div>
     )
 }

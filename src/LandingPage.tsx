6ec3a85f8f0cb8b30239ef09019d833a017b59f2
Simon <s173255@student.dtu.dk>
2022-02-28 15:17:15 +0100

    https test5

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,27 +1,14 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import { Container } from "react-bootstrap";
 
 const LandingPage = () => {
-  const { isAuthenticated, user } = useAuth0();
-
-  if (!isAuthenticated) {
-    return (
-      <Container>
-        <h1>Landing Page</h1>
-        <p>Testing stuff</p>
-        <p>Github Runner works!</p>
-      </Container>
-    );
-  } else {
-    return (
-      <Container>
-        <h1>Landing Page</h1>
-        <p>Testing stuff</p>
-        <p>Github Runner works!</p>
-        <p>{JSON.stringify(user)}</p>
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <h1>Landing Page</h1>
+      <p>Testing stuff</p>
+      <p>Github Runner works!</p>
+      <p>This should update</p>
+    </Container>
+  );
 };
 
 export default LandingPage;

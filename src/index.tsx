1b2a5135ca0485de53cd643eaa4a5b7b0c1de348
Simon Søhår <s173255@student.dtu.dk>
2022-03-01 11:59:07 +0100

    Undo WebSocket

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Auth0Provider } from "@auth0/auth0-react";
-var wss = new WebSocket("wss://api.uglyrage.com")
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider

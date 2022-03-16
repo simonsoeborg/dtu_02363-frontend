af5b5f2914cd3d504738378533bd5b653a376bb6
Kasper Baun <kasperbaun@hotmail.com>
2022-03-16 11:31:45 +0100

    Update EasyTap.tsx
    Quick dirty fix to enter Order page by typing 4 random numbers and pressing E

--- src/EasyTapApp/EasyTap.tsx
+++ src/EasyTapApp/EasyTap.tsx
@@ -1,15 +1,30 @@
 import { useState } from "react";
 import { Container } from "react-bootstrap";
 import LoginApp from "./Login/LoginApp";
+import Order from "./Order/Order";
 
 const MainApp = () => {
 
   const [isLoggedIn, setIsLoggedIn ] = useState(false);
   // console.log(`isLoggedIn: ${isLoggedIn.toString()}`)
+  if(!isLoggedIn){
+    return (
+      <Container fluid>
+          <LoginApp isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+      </Container>
+    );
+  }
+
+  if(isLoggedIn){
+    return (
+      <Container fluid>
+          <Order/>
+      </Container>
+    );
+  }
+
   return (
-    <Container fluid>
-        <LoginApp isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-    </Container>
+    <div></div>
   );
 };
 

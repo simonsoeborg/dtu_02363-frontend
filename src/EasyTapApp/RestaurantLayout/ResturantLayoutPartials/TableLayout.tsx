765bc46b52ad630bce53edb38afeaffb838a48a9
Kristoffer <s180500@win.dtu.dk>
2022-03-16 14:33:25 +0100

    bedre navigation under tablelayout
    mangler at tag tableId med sig

--- src/EasyTapApp/RestaurantLayout/ResturantLayoutPartials/TableLayout.tsx
+++ src/EasyTapApp/RestaurantLayout/ResturantLayoutPartials/TableLayout.tsx
@@ -1,17 +1,32 @@
-import { Nav } from "react-bootstrap";
+import { Nav, Card } from "react-bootstrap";
 import "../../../resources/Css/layout.css";
 import Order from "../../Order/Order";
-
+import { useNavigate } from "react-router-dom";
 interface IProps {
   tableId: number;
   image: any;
 }
 
 const TableLayout = (props: IProps) => {
+  const navigate = useNavigate();
+
+  const toOrderPage = () => {
+    navigate("/EasyTap/Order");
+  };
+
   return (
-    <Nav.Link href="/EasyTap/Order">
-      <img className="TableSize" src={props.image}></img>
-    </Nav.Link>
+    <Card
+      className="bg-transparent text-white"
+      style={{ width: "180px" }}
+      onClick={() => {
+        toOrderPage();
+      }}
+    >
+      <Card.Img src={props.image} className="TableSize" />
+      <Card.ImgOverlay>
+        <Card.Title>{props.tableId}</Card.Title>
+      </Card.ImgOverlay>
+    </Card>
   );
 };
 

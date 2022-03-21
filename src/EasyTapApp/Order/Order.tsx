29513058cfee6b47609180bb95ec47d01acdea9b
Kristoffer <s180500@win.dtu.dk>
2022-03-21 12:43:48 +0100

    Tilføjet visual knapper i Order
    De er uden function. Men her skal tilkobling af print order og sende temptOrder til databasen ske

--- src/EasyTapApp/Order/Order.tsx
+++ src/EasyTapApp/Order/Order.tsx
@@ -8,27 +8,27 @@ import ItemModel from "../../Models/ItemModel";
 import OrderModel from "../../Models/OrderModel";
 import { cs } from "../../Stores/CategoryStore";
 import { is } from "../../Stores/ItemStore";
-import { os } from "../../Stores/OrderStore";
 import Loading from "../../Partials/Loading";
 import { observer } from "mobx-react-lite";
 import TapOutModel from "../../Models/TapOutModel";
 import OrderAmountPanel from "./OrderAmountPanel";
- 
+import PrintBillButton from "./PrintBillButton";
+import TapOutButton from "./TapOutButton";
 
 //Hardcoded tableNumber, but should get tableNumber from an onClick function earlier.
 const Order = () => {
   const [categories, setCategories] = useState<CategoryModel[]>([]);
   const [selectedCategory, setSelectedCategory] = useState<String>("Starters");
   const [hasLoaded, setHasLoaded] = useState(false);
-  const [tableNr, setTableNr] = useState(0);
   const [items, setItems] = useState<ItemModel[]>([]);
 
   // const containing the current order
   const [order, setOrder] = useState<OrderModel>(new OrderModel());
   // const containing the items for the current order
   const [orderItems, setOrderItems] = useState<ItemModel[]>([]);
-  // 
+  //
   const [AmountChosen, setAmount] = useState(0);
+  const [isPayed, setIsPayed] = useState(false);
 
   if (!cs.Categories && !is.Items) {
     return <Loading />;
@@ -71,21 +71,32 @@ const Order = () => {
                 selectedCategory={selectedCategory}
                 orderItems={orderItems}
                 setOrderItems={setOrderItems}
-                amountChosen = {AmountChosen} 
-                setAmount = {setAmount}
+                amountChosen={AmountChosen}
+                setAmount={setAmount}
               />
             </Row>
           </Col>
           <Col>
             <Row>
-              <OrderDisplayOverView 
-              currentOrderItems={orderItems}
-              setCurrentOrderItems={setOrderItems}
+              <OrderDisplayOverView
+                currentOrderItems={orderItems}
+                setCurrentOrderItems={setOrderItems}
               />
-              <OrderAmountPanel 
-              amountChosen = {AmountChosen}
-              setAmount = {setAmount}
-              />
+              <Col>
+                <OrderAmountPanel
+                  amountChosen={AmountChosen}
+                  setAmount={setAmount}
+                />
+              </Col>
+              <Col>
+                <Row className="d-flex justify-content-center">
+                  <PrintBillButton
+                    printbutton={isPayed}
+                    setPrintOut={setIsPayed}
+                  />
+                  <TapOutButton />
+                </Row>
+              </Col>
             </Row>
           </Col>
         </Row>
--- src/EasyTapApp/Order/OrderDisplayCategories.tsx
+++ src/EasyTapApp/Order/OrderDisplayCategories.tsx
@@ -1,30 +1,31 @@
 import { Container, Nav } from "react-bootstrap";
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction } from "react";
 import CategoryModel from "../../Models/CategoryModel";
 
 interface IProps {
-
-  categories : CategoryModel[],
-  setSelectedCategory : Dispatch<SetStateAction<String>>
+  categories: CategoryModel[];
+  setSelectedCategory: Dispatch<SetStateAction<String>>;
 }
 
-const DisplayCategories = (props : IProps) => {
+const DisplayCategories = (props: IProps) => {
+  const handleOnClickEvent = (selectedCategory: string) => {
+    props.setSelectedCategory(selectedCategory);
+  };
 
-    const handleOnClickEvent = (selectedCategory: string) => {
-      props.setSelectedCategory(selectedCategory)
-    }
-
-    return (
-      <Container>
-        <Nav fill variant ="tabs" defaultActiveKey="">
-          {props.categories.map((category, index) => (
-            <Nav.Item key={index} onClick={()=>handleOnClickEvent(category.name)}>
-              <Nav.Link eventKey={category.name}>{category.name}</Nav.Link>
-            </Nav.Item>
-          ))}
-        </Nav>
-      </Container>
-    );
+  return (
+    <Container>
+      <Nav fill variant="tabs" defaultActiveKey="">
+        {props.categories.map((category, index) => (
+          <Nav.Item
+            key={index}
+            onClick={() => handleOnClickEvent(category.name)}
+          >
+            <Nav.Link eventKey={category.name}>{category.name}</Nav.Link>
+          </Nav.Item>
+        ))}
+      </Nav>
+    </Container>
+  );
 };
 
 export default DisplayCategories;

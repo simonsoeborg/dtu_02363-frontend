096338e2eaf8ea57af7f10119cabfc4ed25acbb8
Kristoffer <s180500@win.dtu.dk>
2022-04-25 11:35:07 +0200

    tilføjet images i service som skal bruges

--- src/Services/_services.tsx
+++ src/Services/_services.tsx
@@ -1,10 +1,55 @@
 const API_URL = "https://api.uglyrage.com/api";
 
-    // private api = "https://localhost:5001/api";
-    // "https://api.uglyrage.com/api";
+// private api = "https://localhost:5001/api";
+// "https://api.uglyrage.com/api";
 
-const defaultImage = "https://thumbs.dreamstime.com/b/default-avatar-profile-vector-user-profile-default-avatar-profile-vector-user-profile-profile-179376714.jpg";
-export {
-    API_URL,
-    defaultImage
-};
+const SmokedSalmon =
+  "https://www.eatwell101.com/wp-content/uploads/2020/04/Smoked-Salmon-Toasts-with-Cream-Cheese-Recipe-10.jpg";
+const PullPorkBurger =
+  "https://foodhub.scene7.com/is/image/woolworthsltdprod/1906-pulled-pork-burgers:Desktop-1300x658";
+const pommes =
+  "https://res.cloudinary.com/norgesgruppen/images/c_scale,dpr_auto,f_auto,q_auto:eco,w_1600/wzeatefaswdawcdganc4/hjemmelagde-pommes-frites";
+
+const mayo = "https://m.media-amazon.com/images/I/71l9tod1lTL._SL1500_.jpg";
+
+const brownie = "https://images.matprat.no/5das2kc3et-normal/710";
+
+const newYearSpecial =
+  "https://menugo.com/thumbnails?templateId=ba92cf91-c7e4-4062-bce4-0c041a3d55ce&height=800&lastUpdated=1547562911000";
+
+const cola =
+  "https://logos-world.net/wp-content/uploads/2020/03/Coca-Cola-Logo.png";
+
+const max =
+  "https://i.pinimg.com/originals/8e/a0/cd/8ea0cddeb5208947bcfe290d947c1d39.jpg";
+
+const pilsner =
+  "https://yoolando.com/media/catalog/product/cache/40c697145c30e502f98aaaeebcdb4a02/4/2/42af4b5c117a0c414f05a414d18c5b2da477c399_067423800_1579217587_6740700999475_gr__d64e.jpg";
+
+const classic =
+  "https://nordicexpatshop.com/media/catalog/product/cache/a75b4628650e2182ad447c229a356118/e/4/e4c1c7262a75f63640f2ee765d448b2ee63a256c4ec477743ab930837664d822.jpeg";
+
+const jackDAndCola =
+  "https://cdn.diffords.com/contrib/bws/2017/10/59db8f6a2a274.jpg";
+
+const GAndT =
+  "https://static.independent.co.uk/2021/08/02/13/iStock-994492332.jpg?quality=75&width=1200&auto=webp";
+
+const coffee =
+  "https://www.tastingtable.com/img/gallery/coffee-brands-ranked-from-worst-to-best/l-intro-1645231221.jpg";
+
+const latte =
+  "https://www.nespresso.com/ncp/res/uploads/recipes/nespresso-recipes-Latte-Macchiato.jpg";
+
+const cappuccino =
+  "https://coffeeaffection.com/wp-content/uploads/2021/02/does-a-cappuccino-have-caffeine.jpg";
+
+const irishCoffee =
+  "https://hips.hearstapps.com/hmg-prod/images/1519246658-irish-coffee-delish-1620163679.jpeg";
+
+const doubleupImage =
+  "https://wirelessdmx.com/wp-content/uploads/2019/06/Screenshot-2019-06-28-at-09.30.22-1024x571.png";
+const defaultImage =
+  "https://thumbs.dreamstime.com/b/default-avatar-profile-vector-user-profile-default-avatar-profile-vector-user-profile-profile-179376714.jpg";
+
+export { API_URL, defaultImage, SmokedSalmon };

34d7757a4733e746d63dc1218cc624bc1a6c7531
Kasper Baun <kasperbaun@hotmail.com>
2022-05-02 10:52:20 +0200

    flot billede

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,4 +1,4 @@
-import { Container } from "react-bootstrap";
+import { Container, Image } from "react-bootstrap";
 import NotAuthorized from "./Components/Global/NotAuthorized";
 import { authToken } from "./Stores/AuthTokenStore";
 import { authentication } from "./Stores/AuthenticationStore";
@@ -16,7 +16,7 @@ const LandingPage = () => {
   return (
     <Container>
       <Container>
-        <h1>Landing Page</h1>
+         <Image fluid src={require('../src/resources/landingPageImage.png')}/>
       </Container>
       { authToken.getRole() === "waiter" && (
         <NotAuthorized message="Waiters are restricted from viewing this page!" navUrl="/EasyTap"/>

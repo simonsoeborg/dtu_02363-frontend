1240445b74fff334ba7187159095d00129bae554
Kristoffer <s180500@win.dtu.dk>
2022-05-02 11:18:06 +0200

    tilføjet små ændringer til image, så det er mere dynamisk

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,28 +1,43 @@
-import { Container, Image } from "react-bootstrap";
+import { Container, Card } from "react-bootstrap";
 import NotAuthorized from "./Components/Global/NotAuthorized";
 import { authToken } from "./Stores/AuthTokenStore";
 import { authentication } from "./Stores/AuthenticationStore";
 import { useAuth0 } from "@auth0/auth0-react";
+import LandingImage from "../src/resources/landingPageImage.png";
 
 const LandingPage = () => {
   const { isAuthenticated } = useAuth0();
-  let collect : any = localStorage.getItem("authValues")
-  if( isAuthenticated ) {
-    if(collect !== "\"\"" && collect.length > 0) {
-      let data = JSON.parse(collect!!)
+  let collect: any = localStorage.getItem("authValues");
+  if (isAuthenticated) {
+    if (collect !== '""' && collect.length > 0) {
+      let data = JSON.parse(collect!!);
       authentication.decodeJWT(data);
     }
   }
   return (
     <Container>
-      <Container>
-         <Image fluid src={require('../src/resources/landingPageImage.png')}/>
+      <Container fluid={true} className="p-0">
+        <Card>
+          <Card.Img
+            src={LandingImage}
+            alt="LandingImage"
+            className="img-fluid"
+            variant="top"
+            style={{
+              width: "auto",
+              height: "100%",
+            }}
+          />
+        </Card>
       </Container>
-      { authToken.getRole() === "waiter" && (
-        <NotAuthorized message="Waiters are restricted from viewing this page!" navUrl="/EasyTap"/>
+      {authToken.getRole() === "waiter" && (
+        <NotAuthorized
+          message="Waiters are restricted from viewing this page!"
+          navUrl="/EasyTap"
+        />
       )}
     </Container>
-  )
+  );
 };
 
 export default LandingPage;
